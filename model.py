c7ca2d6add590cfc4cc6473b3c71b49520b09eeb
AditMeh <aditmeh123@gmail.com>
2022-09-04 04:22:41 -0400

    finally, it finally works. I am very happy

--- model.py
+++ model.py
@@ -3,16 +3,18 @@ import torch
 
 
 class NerfModel(nn.Module):
-    def __init__(self, position_dim):
+    def __init__(self, freqs):
         super(NerfModel, self).__init__()
         """
         position_dim: The dimension of the last axis of the points
         direction_dim: The dimension of the last axis of the direction
         """
-        self.position_dim = position_dim
+        self.freqs = freqs
+
+        self.posenc_dim = 3 + 3*2*freqs
 
         self.model_1 = nn.Sequential(
-            nn.Linear(position_dim, 256),
+            nn.Linear(self.posenc_dim, 256),
             nn.ReLU(),
             nn.Linear(256, 256),
             nn.ReLU(),
@@ -26,7 +28,7 @@ class NerfModel(nn.Module):
 
         # concatenate with the position vector
         self.model_2 = nn.Sequential(
-            nn.Linear(256 + position_dim, 256),
+            nn.Linear(256 + self.posenc_dim, 256),
             nn.ReLU(),
             nn.Linear(256, 256),
             nn.ReLU(),
@@ -49,6 +51,7 @@ class NerfModel(nn.Module):
 
     def forward(self, position):
         position_flat = torch.reshape(position, (-1, 3))
+        position_flat = self.posenc(position_flat)
 
         intermediate_rep = self.model_1(position_flat)
 
@@ -62,11 +65,15 @@ class NerfModel(nn.Module):
 
         return torch.reshape(rgb, position.shape), torch.reshape(density, position.shape[:-1])
     def posenc(self, t):
-
+        terms = [t]
+        for i in range(self.freqs):
+            for fn in [torch.sin, torch.cos]:
+                terms.append(fn(2.**i * t))
+        return torch.concat(terms, axis = -1)
 
 if __name__ == "__main__":
 
-    nerf_model = NerfModel(3)
+    nerf_model = NerfModel(6)
 
     points = torch.ones(size=(50, 50, 64, 3))
 
--- test.py
+++ test.py
@@ -17,24 +17,28 @@ if __name__ == "__main__":
 
     images, poses, focal, w, h = load_data('tiny_nerf_data.npz')
 
-    idx = 0
-    image =images[idx]
+    idx = 2
+    image = images[idx]
     pose = poses[idx]
     rotation = pose[0:3, 0:3]
     translation = pose[0:3, 3]
 
-    near = 3
+    near = 2
     far = 6
-    samples_num = 30
+    samples_num = 40
 
     dataset = TinyDataset(images, poses, focal, w, h, near, far, samples_num)
 
     points = dataset[idx]
 
-    nerf_model = NerfModel(3)
+    device = (torch.device('cuda') if torch.cuda.is_available()
+              else torch.device('cpu'))
 
-    epochs = 40
-    lr = 1e-3
+    nerf_model = NerfModel(3).to(device=device)
+    points = points.to(device=device)
+
+    epochs = 500
+    lr = 5e-4
     optimizer = torch.optim.Adam(nerf_model.parameters(), lr=lr)
     for epoch in tqdm.tqdm(range(epochs)):
         rgbs, density = nerf_model(points)
@@ -47,13 +51,15 @@ if __name__ == "__main__":
 
         rendered_img = torch.reshape(C, (h, w, 3))
 
-        mse = nn.MSELoss(reduction='sum')(torch.tensor(image), rendered_img)
+        mse = nn.MSELoss(reduction='sum')(
+            torch.tensor(image).to(device=device), rendered_img)
         print('\nepoch', epoch + 1, ': ', mse)
         optimizer.zero_grad()
         mse.backward()
         optimizer.step()
 
-    f, axarr = plt.subplots(1,1)
+    f, axarr = plt.subplots(1, 1)
 
-    axarr.imshow(torch.reshape(C, (h, w, 3)).detach().numpy())
+    axarr.imshow(torch.reshape(C, (h, w, 3)).cpu().detach().numpy())
+    plt.savefig("vis.png")
     plt.show()
